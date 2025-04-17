39a099879dded9c4bf56514d57017ddb2b8994df
Mayank <mayankanand.mi@gmail.com>
2025-04-17 12:32:49 +0530

    Added prediction script

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -1,30 +1,40 @@
-from pathlib import Path
-
-from loguru import logger
-from tqdm import tqdm
-import typer
-
+import pickle
+import pandas as pd
+import xgboost as xgb
 from src.config import MODELS_DIR, PROCESSED_DATA_DIR
 
-app = typer.Typer()
 
+def load_data(data_path, processed_util_path):
+    test_data = pd.read_csv(data_path)
+    index_test_data = test_data[['FarmerID']]
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
+    with open(processed_util_path, 'rb') as f:
+        processing_util = pickle.load(f)
+
+    categorical_cols = processing_util['categorical_columns']
+
+    test_dmatrix = test_data.drop(['FarmerID'], axis=1)
+    test_dmatrix = xgb.DMatrix(test_dmatrix)
+    return test_dmatrix, index_test_data
+
+def load_model(model_path):
+    model = xgb.Booster()
+    model.load_model(model_path)
+    return model
 
 
 if __name__ == "__main__":
-    app()
+    exp_name = 'exp_v1'
+    test_data_path = PROCESSED_DATA_DIR / exp_name / 'test_v0.csv'
+    processed_util_path = PROCESSED_DATA_DIR / exp_name / 'processing_util.pkl'
+    model_path = MODELS_DIR / exp_name / 'income_regression_model.bst'
+
+    print('Load Dataset')
+    test_dmatrix, index_test_data = load_data(data_path=test_data_path, processed_util_path=processed_util_path)
+    
+    print('Load model')
+    model = load_model(model_path=model_path)
+    y_pred = model.predict(test_dmatrix)
+    index_test_data['farmer_pred_income'] = y_pred
+
+    index_test_data.to_csv('test_submission.csv', index=False)
